ddf72d4b7cc36d974c7a3c3f5684aa511fe7b952
duvan <duvan318@gmail.com>
2019-11-14 23:54:21 -0500

    Multiproceso de lectura de pulsos creada. Creación de settings.

---
+++ pulsed_sensor.py
@@ -0,0 +1,66 @@
+'''
+
+pulsed_sensor.py
+
+Read GPIO port and count detected pulses, calculate a sample every
+specified time and save in database mysql and in a local directory.
+
+'''
+
+import RPi.GPIO as GPIO
+import time
+import multiprocessing
+import os
+import settings
+import json
+import datetime
+
+
+def monitorchannel(channel, bouncetime, counters):
+    '''
+    Function used in a multiprocessing thread to count pulses
+    detected in a specified GPIO port stablished in setting.json file
+    '''
+    last_edge = time.time()
+    while True:
+        if  GPIO.input(channel):
+            current_edge = time.time()
+            if current_edge - last_edge > bouncetime:
+                counters[0] +=1
+                print('Counter pulses: %s' % counters[0])
+            else:
+                pass
+            last_edge = current_edge
+        time.sleep(bouncetime/10)
+
+
+if __name__ == "__main__":
+    
+    # Open Settings in variable 'S'
+    S = settings.read_json('settings.json')
+    CONFIG = S['CONFIG']
+    DATABASE = S['DATABASE']
+
+    # Setup pin configuration
+    pin_sensor = CONFIG['GPIO_PIN_SENSOR'] # select GPIO pin
+    bouncetime_sensor = CONFIG['BOUNCETIME'] # avoid parasites pulses
+    resolution = CONFIG['RESOLUTION_PER_PULSE'] # physical pulse unit
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin_sensor, GPIO.IN)
+    
+    print('Pin sensor: ')
+    print(pin_sensor)
+    print('bouncetime_sensor: ')
+    print(bouncetime_sensor)
+    
+    counters = multiprocessing.Array('d',[0]*2)
+    #values = multiprocessing.Array('d',[0]*2)
+    #newdata = multiprocessing.Array('i',[0]*4)
+    
+    # Processes to monitor pulsed input
+    monitor_process = multiprocessing.Process(target = monitorchannel,
+                                            args = (pin_sensor,
+                                                    bouncetime_sensor,
+                                                    counters))
+    monitor_process.start()
+        
---
+++ settings.py
@@ -0,0 +1,19 @@
+'''
+File that read json settings file and return the values
+'''
+
+import json
+    
+
+def read_json(filename):
+    ''' Read json file specified in filename param. '''
+    with open(filename,'r') as file:
+            settings = json.load(file)
+    return settings
+
+
+if __name__ == '__main__':
+    
+    settings = read_json('settings.json')
+    print(settings)
+    
